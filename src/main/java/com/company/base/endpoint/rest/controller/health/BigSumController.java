a4def9086be8c02bc96ca2923cf0750b26cb8131
Cedric <99885260+Cenikl@users.noreply.github.com>
2023-12-21 09:27:15 +0300

    fix: gig sum controller

--- src/main/java/com/company/base/endpoint/rest/controller/health/BigSumController.java
+++ src/main/java/com/company/base/endpoint/rest/controller/health/BigSumController.java
@@ -8,6 +8,7 @@ import lombok.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import java.math.BigInteger;
 
